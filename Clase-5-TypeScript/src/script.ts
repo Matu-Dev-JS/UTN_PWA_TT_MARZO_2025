624c95af4ce20d78b1a569a87c76e734ee9ef7d0
matu.dev <mati.a.gimenez2002@gmail.com>
2025-03-27 18:15:26 -0300

    clase 6

--- Clase-5-TypeScript/src/script.ts
+++ Clase-5-TypeScript/src/script.ts
@@ -12,37 +12,37 @@
 //funciones y variables globales o locales
 
 //Objetos: Metodos y propiedades
-
+/* 
 let nombre = 'pepe'
 
 const saludar = (nombre: string) => {
     console.log(`Hola ${nombre}`)
-}
+} */
 
 //Objeto literal
-const auto = {
+/* const auto = {
     marca: 'Fiat',
     color: 'rojo',
     distancia_recorrida: 0
-}
+} */
 
 //Funcion
-const recorrerDistancia = (distancia: number, vehiculo: any) => {
+/* const recorrerDistancia = (distancia: number, vehiculo: any) => {
     vehiculo.distancia_recorrida += distancia
 }
 
 recorrerDistancia(10, auto)
-
+ */
 //class, constructor, this, new
 
 //class : cuando queremos crear una nueva clase usamos la palabra class (se recomienda que el nombre de la clase sea en mayuscula) (UpperCammelCase)
 //Las class crean objetos
 //Ejemplo de crear una class
-class DummyClass {
+/* class DummyClass {
     constructor(){
         
     }
-}
+} */
 
 //constructor
 //Cuando yo instancie la clase se invocara a la funcion contructora de dicha clase
@@ -50,7 +50,7 @@ class DummyClass {
 //Que es instanciar? es similar a invocar a una funcion, pero en una clase
 //Ejemplo de instanciar
 
-new DummyClass()
+/* new DummyClass()
 
 class Vehiculo {
     marca: string
@@ -67,12 +67,12 @@ class Vehiculo {
     getDistanciaRecorrida (){
         return this.distancia_recorrida
     }
-}
+} */
 
 //Instancio la class Vehiculo
-const autoRojo = new Vehiculo('Ford', 'Rojo')
+/* const autoRojo = new Vehiculo('Ford', 'Rojo')
 autoRojo.aumentarDistancia(80)
-console.log(autoRojo.getDistanciaRecorrida())
+console.log(autoRojo.getDistanciaRecorrida()) */
 
 /* const persona = {
     nombre: 'maria',
@@ -82,3 +82,73 @@ console.log(autoRojo.getDistanciaRecorrida())
 }
 
 persona.saludar('pepe') */
+
+
+
+
+//Crear un gestor de tareas
+
+//Tareas
+/* 
+    -id
+    -titulo
+    -completada
+    -fecha_creacion
+*/
+//Gestor
+/* 
+    -tareas : Tarea[]
+
+    methods: 
+
+    -agregar() Create
+    -mostrarTareas() Read
+    -eliminarTodasLasTareas() Delete
+    -completarTareaPorId()
+    -mostrarTareasCompletadas()
+
+    -actualizar() Update (No vamos a llegar)
+*/
+
+//Inferencia de datos
+let nombre  = 'pepe'
+
+
+
+class Tarea {
+    id: number
+    titulo: string
+    completada: boolean
+    fecha_creacion: Date
+
+    constructor(
+        id: number, 
+        titulo: string, 
+        completada : boolean = false, 
+        fecha_creacion : Date = new Date()
+    ){
+        this.id = id
+        this.titulo = titulo
+        this.completada = completada
+        this.fecha_creacion = fecha_creacion
+    }
+}
+
+
+class Gestor {
+    tareas: Tarea[]
+    constructor(
+        tareas: Tarea[] = []
+    ){
+        this.tareas = tareas
+    }
+
+    agregar(titulo: string, completada: boolean) : void{
+        const nueva_tarea : Tarea = new Tarea(
+            this.tareas.length, 
+            titulo, 
+            completada
+        )
+        this.tareas.push(nueva_tarea)
+    }
+}
