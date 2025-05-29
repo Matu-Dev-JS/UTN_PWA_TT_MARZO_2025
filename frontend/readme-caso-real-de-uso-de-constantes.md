1b66e2649ac28d5400bd087a6c1d29630b8b5d83
matu.dev <mati.a.gimenez2002@gmail.com>
2025-05-29 17:24:26 -0300

    Clase 21

---
+++ frontend/readme-caso-real-de-uso-de-constantes.md
@@ -0,0 +1,7 @@
+//Validacion facial de AWS (Rekognition)
+//Te retornaba un number de score
+//1 y 10000 (1 es poco probable y 10000 es muy probable)
+//const SCORE_MIN_HUMAN = 9000
+//let isHuman = user.score >= SCORE_MIN_HUMAN
+
+//UNA de las ventajas de usar constantes es poder hacer mas legible el codigo
--- frontend/src/Screens/RegisterScreen/RegisterScreen.jsx
+++ frontend/src/Screens/RegisterScreen/RegisterScreen.jsx
@@ -1,10 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './RegisterScreen.css'
+import useForm from '../../hooks/useForm'
+import { REGISTER_FIELD_NAMES } from '../../constants/form/register'
+import { useNavigate } from 'react-router-dom'
+import { register } from '../../services/authService'
 
 const RegisterScreen = () => {
-  return (
-    <div>RegisterScreen</div>
-  )
+	const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const navigate = useNavigate()
+	const onSubmit = async () => {
+		try {
+			setLoading(true)
+			const server_response_data = await register({
+				name: form_state[REGISTER_FIELD_NAMES.NAME],
+				email: form_state[REGISTER_FIELD_NAMES.EMAIL],
+				password: form_state[REGISTER_FIELD_NAMES.PASSWORD]
+			})
+			if (server_response_data.ok) {
+				navigate('/login')
+			}
+			else {
+				setError(server_response_data.message)
+			}
+		}
+		catch (error) {
+			console.log(error)
+			setError('Ocurrio un error al comunicarnos con el servidor (intentalo mas tarde)')
+		}
+		finally {
+			setLoading(false)
+		}
+	}
+	const {form_state, handleSubmit, handleChange} = useForm({ 
+		onSubmit, 
+		initial_form_state: { 
+			[REGISTER_FIELD_NAMES.NAME]: '', 
+			[REGISTER_FIELD_NAMES.EMAIL]: '', 
+			[REGISTER_FIELD_NAMES.PASSWORD]: '' 
+		} 
+	})
+
+	return (
+		<div>
+			<h1>Registro</h1>
+			<form onSubmit={handleSubmit}>
+
+				<div>
+					<label htmlFor='name'>Ingresa tu nombre:</label>
+					<input
+						id='name'
+						name={REGISTER_FIELD_NAMES.NAME}
+						type='text'
+						value={form_state[REGISTER_FIELD_NAMES.NAME]}
+						onChange={handleChange}
+
+					/>
+				</div>
+				<div>
+
+					<label htmlFor='email'>Ingresa tu mail:</label>
+					<input
+						id='email'
+						name={REGISTER_FIELD_NAMES.EMAIL}
+						placeholder='joedoe@mail.com'
+						type='email'
+						value={form_state[REGISTER_FIELD_NAMES.EMAIL]}
+						onChange={handleChange}
+					/>
+				</div>
+				<div>
+					<label htmlFor='password'>Ingresa tu Contraseña:</label>
+					<input
+						id='password'
+						name={REGISTER_FIELD_NAMES.PASSWORD}
+						type='password'
+						value={form_state[REGISTER_FIELD_NAMES.PASSWORD]}
+						onChange={handleChange}
+
+					/>
+				</div>
+				{error && <span style={{ color: 'red' }}>{error}</span>}
+				{
+					loading
+						? <button type='button' disabled={loading}>Cargando</button>
+						: <button type='submit' >Registrar</button>
+				}
+
+			</form>
+		</div>
+	)
 }
 
 export default RegisterScreen
